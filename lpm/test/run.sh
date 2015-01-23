4ca73ca27f3c714f4e6fb60042d52cc3ca2054d8
Lukáš Vokráčko <lukas@vokracko.cz>
2015-01-23 18:42:43 +0100

    Valgrind

--- lpm/test/run.sh
+++ lpm/test/run.sh
@@ -5,17 +5,23 @@ for testfile in `ls $1`
 do
 	./cmd < $1/$testfile
 
-	if [ $? -eq 1 ]; then
+	if [ $? -eq 0 ]; then
+		valgrind ./cmd < $1/$testfile 2>&1 | grep 'no leaks' > /dev/null
+
+		if [ $? -eq 0 ]; then
+			echo -e " \033[1;32mPASS\033[0m, valgrind: \033[1;32mPASS\033[0m $1/$testfile"
+			let "pass++"
+		else
+			echo -e " \033[1;32mPASS\033[0m, valgrind: \033[1;31mFAIL\033[0m $1/$testfile"
+			let "fail++"
+		fi
+	else
 		echo -e " \033[1;31mFAIL\033[0m $1/$testfile"
 		let "fail++"
-		# $((fail++))
-	else
-		echo -e " \033[1;32mPASS\033[0m $1/$testfile"
-		let "pass++"
 	fi
 
 done
 
-echo -e " \033[1;31m$fail\033[0m/\033[1;32m$pass\033[0m"
+echo -e " \033[1;31mFAIL: $fail\033[0m, \033[1;32mPASS: $pass\033[0m"
 
 
