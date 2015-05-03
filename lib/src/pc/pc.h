29fb4e18824ab1fadb583fba6a79c5bd7a129f4d
Lukáš Vokráčko <lukas@vokracko.cz>
2015-05-03 13:38:26 +0200

    ipv6

--- lib/src/pc/pc.h
+++ lib/src/pc/pc.h
@@ -9,7 +9,17 @@ typedef struct
 	short port;
 } pc_set;
 
+typedef struct
+{
+	unsigned rule;
+	struct in_addr dst;
+	struct in_addr src;
+	_Bool protocol;
+	short port;
+} pc6_set;
+
 typedef void pc_root;
+typedef void pc6_root;
 
 pc_root * pc_init();
 void pc_destroy(pc_root * root);
@@ -17,3 +27,8 @@ _Bool pc_add(pc_root * root, pc_set set[], unsigned count);
 _Bool pc_update(pc_root * root, pc_set old, pc_set new);
 void pc_remove(pc_root * root, pc_set set);
 
+pc6_root * pc6_init();
+void pc6_destroy(pc6_root * root);
+_Bool pc6_add(pc6_root * root, pc6_set set[], unsigned count);
+_Bool pc6_update(pc6_root * root, pc6_set old, pc6_set new);
+void pc6_remove(pc6_root * root, pc6_set set);
